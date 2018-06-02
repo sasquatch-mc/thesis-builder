d7b38a8b0e1cc6f3236d8f9554f041e892c26b7f
Yoan-Alexander Grigorov <joan.grigorov@gmail.com>
2018-06-02 16:50:54 +0200

    Initial commit

---
+++ config/mail.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    'driver' => env('MAIL_DRIVER', 'smtp'),
+    'host' => env('MAIL_HOST', 'smtp.gmail.com'),
+    'port' => env('MAIL_PORT', 587),
+    'from' => [
+        'address' => env('MAIL_FROM_ADDRESS'),
+        'name' => env('MAIL_FROM_NAME'),
+    ],
+    'encryption' => env('MAIL_ENCRYPTION', 'tls'),
+    'username' => env('MAIL_USERNAME'),
+    'password' => env('MAIL_PASSWORD'),
+
+    'markdown' => [
+        'theme' => 'default',
+        'paths' => [
+            resource_path('views/vendor/mail'),
+        ],
+    ],
+
+];
